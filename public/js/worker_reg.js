de9b470c531c87c7d62864dbeb08dfadfbae9afa
Yaroslav <AzisHop777@mail.ru>
2021-02-28 12:20:31 +0300

    Reg dynamic tmp done

--- public/js/worker_reg.js
+++ public/js/worker_reg.js
@@ -1,19 +1,15 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function () {
-    myFunction()
-};
+const application = document.getElementById('worker_reg');
+const clk = document.getElementById('clk')
 
-// Get the navbar
-var navbar = document.getElementById("myTopnav");
+// function RegPage() {
+//     application.innerHTML = '';
+//     application.innerHTML = template();
+// }
 
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-    } else {
-        navbar.classList.remove("sticky");
-    }
+clk.onclick = function() {
+    application.innerHTML = '';
+    application.innerHTML = template();
 }
+
+// profilePage();
+//RegPage();
