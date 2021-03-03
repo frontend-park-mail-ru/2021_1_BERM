0d80f36cfd1b88e3b77002dc4e2fb59075286563
Алексей Егоров <egorovaleksey0@mail.ru>
2021-03-03 16:56:56 +0300

    delete js files

--- public/templates/index.js
+++
@@ -1,7 +0,0 @@
-function index(locals) {var pug_html = "", pug_mixins = {}, pug_interp;pug_mixins["indexContent"] = pug_interp = function(){
-var block = (this && this.block), attributes = (this && this.attributes) || {};
-pug_html = pug_html + "\u003Cdiv class=\"main_text\"\u003EМиллион исполнителей готовы вам помочь\u003C\u002Fdiv\u003E\u003Cdiv class=\"menu__buttons\"\u003E\u003Cbutton class=\"menu__button_client\"\u003EНайти исполнителя\u003C\u002Fbutton\u003E\u003Ca class=\"menu__button_work\"\u003EНайти работу\u003C\u002Fa\u003E\u003C\u002Fdiv\u003E";
-};
-pug_html = pug_html + "\u003Cdiv class=\"main\"\u003E\u003Cdiv class=\"main__container\"\u003E\u003Cdiv class=\"main__container_center\"\u003E";
-pug_mixins["indexContent"]();
-pug_html = pug_html + "\u003C\u002Fdiv\u003E\u003C\u002Fdiv\u003E\u003C\u002Fdiv\u003E";;return pug_html;}
--- public/templates/navbar.js
+++
@@ -1,7 +0,0 @@
-function navbar(locals) {var pug_html = "", pug_mixins = {}, pug_interp;pug_mixins["navbarContent"] = pug_interp = function(){
-var block = (this && this.block), attributes = (this && this.attributes) || {};
-pug_html = pug_html + "\u003Ca href=\"#workerReg\"\u003EРегистрация\u003C\u002Fa\u003E\u003Ca href=\"#login\"\u003EВход\u003C\u002Fa\u003E\u003Cbutton class=\"navbar__button_order\"\u003EРазместить заказ\u003C\u002Fbutton\u003E\u003Cdiv class=\"navbar__button_title\"\u003E\u003Ca class=\"active\" href=\"#\"\u003EFL\u003C\u002Fa\u003E\u003C\u002Fdiv\u003E";
-};
-pug_html = pug_html + "\u003Cdiv class=\"navbar\" id=\"myTopnav\"\u003E\u003Cdiv class=\"main__container\"\u003E\u003Cdiv class=\"main__container_center\"\u003E";
-pug_mixins["navbarContent"]();
-pug_html = pug_html + "\u003C\u002Fdiv\u003E\u003C\u002Fdiv\u003E\u003C\u002Fdiv\u003E";;return pug_html;}
