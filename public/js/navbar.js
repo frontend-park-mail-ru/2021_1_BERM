87729fb8e78e3db344bed1399b01a48362306750
Алексей Егоров <egorovaleksey0@mail.ru>
2021-03-07 14:52:40 +0300

    spa

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -4,10 +4,10 @@ window.onscroll = function () {
 };
 
 // Get the navbar
-var navbar = document.getElementById("myTopnav");
+let navbar = document.getElementById("myTopnav");
 
 // Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+let sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
