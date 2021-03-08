ce76435a5ff0fb18d572c0f835270ef9a51a4529
Алексей Егоров <egorovaleksey0@mail.ru>
2021-03-08 18:00:07 +0300

    ajax

---
+++ public/js/modules/ajax.js
@@ -0,0 +1,20 @@
+export default {
+    sendRequest(method, url, body = null) {
+        return fetch(url, {
+            method: method,
+            body: JSON.stringify(body)
+        })
+            .then(res => {
+                if (res.ok) {
+                    return res.json();
+                }
+
+                return res.json()
+                    .then(err => {
+                        const error = new Error('Что-то пошло не так :(');
+                        error.data = err;
+                        throw error;
+                    })
+            })
+    }
+}
