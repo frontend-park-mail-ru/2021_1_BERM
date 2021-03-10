ec3de43a4c67b41db913270a302f06eaec638267
Алексей Егоров <egorovaleksey0@mail.ru>
2021-03-10 09:39:26 +0300

    reg-login

--- public/js/modules/controller.js
+++ public/js/modules/controller.js
@@ -3,6 +3,7 @@ import ajax from "./ajax.js";
 const root = document.getElementById('root');
 let saveData = {
     authorized: true,
+    id: 0,
     img: 'img/profile.jpg',
 };
 
@@ -14,10 +15,9 @@ export default {
             // Todo Запрос изображения. Сохраняем его в saveData
         }
 
-        // eslint-disable-next-line no-undef
         root.innerHTML = navbarTemplate({
             authorized : saveData.authorized,
-            profIcon : saveData.img, // Todo ?
+            profIcon : saveData.img
         }) + indexTemplate();
     },
 
@@ -31,20 +31,26 @@ export default {
             event.preventDefault();
 
             // Todo: Валидация
-
             // Todo Тут добавить функцию валидации
+
             let requestData= { };
             const formData = new FormData(event.target);
 
             for(let [name, value] of formData) {
                 requestData[name] = value;
             }
-            // Todo Поставить нормальный URL и проверить работу
             ajax.sendRequest('POST', 'http://95.163.212.121:8080/signin', JSON.parse(JSON.stringify(requestData)))
             // Todo возвращается стуктуру или ее сохранить или изменить API
                 .then(response => {
-                    if (response.status === undefined) {
+                    if (response!= null && response.status === undefined) {
                         saveData.authorized = true;
+                        saveData.id = response.id;
+                        // email: "pekanbo3333y7@gmail.com"
+                        // executor: false
+                        // first_name: "dasd"
+                        // id: 25
+                        // second_name: "dasasd"
+                        // user_name: "asdssad"
                         this.Route();
                         this.addHandleLinks();
                     }
@@ -63,7 +69,6 @@ export default {
             event.preventDefault();
 
             // Todo: Валидация
-
             // Todo Тут добавить функцию валидации!!
 
             let requestData= { };
@@ -74,15 +79,14 @@ export default {
             }
             requestData.Executor = false;
 
-            console.log(JSON.parse(JSON.stringify(requestData)))
-            let res = ajax.sendRequest('POST', 'http://95.163.212.121:8080/signup', JSON.parse(JSON.stringify(requestData)))
+            ajax.sendRequest('POST', 'http://95.163.212.121:8080/signup', JSON.parse(JSON.stringify(requestData)))
                 // Todo возвращается структуру или ее сохранить или изменить API
                 .then(response => {
-                    if (res!= null && response.status === undefined) {
+                    if (response!= null && response.status === undefined) {
                         this.loginRoute();
+                        alert("Вы успешно зарегались. Войдите."); // Todo убрать
                         this.addHandleLinks();
                     } else {
-                        console.log(res);
                         // Todo Неверный Логин или Пароль или ошибка на сервере
                     }
                 })
@@ -101,7 +105,6 @@ export default {
             event.preventDefault();
 
             // Todo: Валидация
-
             // Todo Тут добавить функцию валидации!!
 
             let requestData= { };
@@ -110,17 +113,14 @@ export default {
             for(let [name, value] of formData) {
                 requestData[name] = value;
             }
-            requestData.Executor = true;
+            requestData.executor = true;
 
-            console.log(JSON.parse(JSON.stringify(requestData)))
-            let res = ajax.sendRequest('POST', 'http://95.163.212.121:8080/signup', JSON.parse(JSON.stringify(requestData)))
-                // Todo возвращается структуру или ее сохранить или изменить API
+            ajax.sendRequest('POST', 'http://95.163.212.121:8080/signup', JSON.parse(JSON.stringify(requestData)))
                 .then(response => {
-                    if (res!= null && response.status === undefined) {
+                    if (response != null && response.status === undefined) {
                         this.loginRoute();
                         this.addHandleLinks();
                     } else {
-                        console.log(res);
                         // Todo Неверный Логин или Пароль или ошибка на сервере
                     }
                 })
@@ -128,33 +128,22 @@ export default {
     },
 
     profileRoute() {
+        // Todo Получить изображение
         const profileInfo = {
             profileImgUrl: "img/profile.jpg",
-            name: "Олег Реуцкий",
-            nickName: "astlok",
-            specialize: "Мобильная разработка",
-            reviews: {
-                all: 25,
-                good: 20,
-                bad: 5,
-            },
-            rating: 5,
-            totalOrders: 30,
-            about: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut non eaque veniam quisquam temporibus nihil id " +
-                "tempora rerum, cumque, aliquid voluptatem nemo cum hic reiciendis obcaecati laudantium fuga quia aperiam. " +
-                "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat impedit cumque voluptate pariatur ipsam " +
-                "expedita temporibus! Dolore inventore veritatis iusto quisquam, laboriosam soluta provident aliquid illum, " +
-                "numquam similique ea voluptate. Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae fugit accusamus " +
-                "aliquam enim molestias voluptatem ipsa Yarik davai delay rekursiy quam voluptatum quibusdam cupiditate, ex eius. Accusamus voluptate " +
-                "veritatis laudantium similique cumque, numquam dolor. Lorem ipsum dolor sit, amet consectetur adipisicing elit. " +
-                "Repellat eos unde dolores ab explicabo eveniet dolorum voluptates quam nesciunt pariatur? Sit iste consectetur, " +
-                "harum ex commodi repellat porro velit ut."
-        } // Временная структура
-        // Todo: Тут запрос профиля
+        }
+        //
+        // if (!saveData.authorized) {
+        //
+        // }
+        // ajax.sendRequest('POST', `http://95.163.212.121:8080/profile/${saveData.id}`)
 
         document.title = 'Профиль';
 
-        root.innerHTML = navbarTemplate() + profileTemplate(profileInfo);
+        root.innerHTML = navbarTemplate({
+            authorized : saveData.authorized,
+            profIcon : saveData.img
+        }) + profileTemplate(profileInfo);
     },
 
     settingsRoute() {
@@ -165,15 +154,13 @@ export default {
             surname: "Реуцкий",
             nickName: "astlok",
             email: "astlok@ya.ru",
-            phone: "2283221488",
         }
 
-        const profNavbar = {
-            authorized: true,
-            profIcon: "img/profIcon.png"
-        }
 
-        root.innerHTML = navbarTemplate(profNavbar) + settingsTemplate(profileSettings)
+        root.innerHTML = navbarTemplate({
+            authorized : saveData.authorized,
+            profIcon : saveData.img
+        }) + settingsTemplate(profileSettings)
 
         // Todo: Валидация
         // Todo: POST запрос настроек
