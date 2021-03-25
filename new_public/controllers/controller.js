d8e742773b716e77b5617869b8471e803f476ee3
Алексей Егоров <egorovaleksey0@mail.ru>
2021-03-25 18:16:42 +0300

    add BaseMVC

--- new_public/controllers/controller.js
+++ new_public/controllers/controller.js
@@ -1,33 +1,4 @@
-import eventBus from '../modules/eventBus.js'
+import {BaseMVC} from "../modules/baseMVC.js";
 
-export class Controller {
-    /**
-     * Добавляет слушателей на события
-     * @author Егоров Алексей
-     */
-    onAll() {
-        if (!this.listeners) {
-            return;
-        }
-
-        this.listeners
-            .forEach((event) => {
-                eventBus.on(event[0], event[1]);
-            });
-    }
-
-    /**
-     * Удаляет слушателей событий
-     * @author Егоров Алексей
-     */
-    offAll() {
-        if (!this.listeners) {
-            return;
-        }
-
-        this.listeners
-            .forEach((event) => {
-                eventBus.off(event[0], event[1]);
-            });
-    }
+export class Controller extends BaseMVC {
 }
---
+++ new_public/modules/baseMVC.js
@@ -0,0 +1,33 @@
+import eventBus from './eventBus.js'
+
+export class BaseMVC {
+    /**
+     * Добавляет слушателей на события
+     * @author Егоров Алексей
+     */
+    onAll() {
+        if (!this.listeners) {
+            return;
+        }
+
+        this.listeners
+            .forEach((event) => {
+                eventBus.on(event[0], event[1]);
+            });
+    }
+
+    /**
+     * Удаляет слушателей событий
+     * @author Егоров Алексей
+     */
+    offAll() {
+        if (!this.listeners) {
+            return;
+        }
+
+        this.listeners
+            .forEach((event) => {
+                eventBus.off(event[0], event[1]);
+            });
+    }
+}
