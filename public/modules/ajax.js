af4573e1eb4d209116fd3d6e4ce8e6968c1e8b01
Алексей Егоров <egorovaleksey0@mail.ru>
2021-04-20 20:20:22 +0300

    change host

--- public/modules/ajax.js
+++ public/modules/ajax.js
@@ -1,7 +1,5 @@
-import {notti} from '../components/notification/notti.js';
-
-const origin = 'http://localhost:8080';
-// const origin = 'https://findfreelancer.ru:8080';
+// const origin = 'http://localhost:8080';
+const origin = 'https://findfreelancer.ru:8080';
 
 let token;
 
