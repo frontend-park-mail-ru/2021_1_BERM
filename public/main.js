68152cc454f56d1f8d3911cc7e8a14e33d220f1b
Алексей Егоров <egorovaleksey0@mail.ru>
2021-04-23 21:52:08 +0300

    .

--- public/main.js
+++ public/main.js
@@ -46,7 +46,7 @@ import {
 } from './modules/utils/pageNames.js';
 
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/public/sw.js')
+    navigator.serviceWorker.register('./public/sw.js')
         .then((registration) => {
             console.log('sw registration on scope:', registration.scope);
         })
