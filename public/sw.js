57e2f0268f6230742b05e762c22e1b2a1de26fbe
Алексей Егоров <egorovaleksey0@mail.ru>
2021-05-05 18:21:33 +0300

    ok

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = 's-app-v2.0';
-const dynamicCacheName = 'd-app-v2.0';
+const staticCacheName = 's-app-v3.0';
+const dynamicCacheName = 'd-app-v3.0';
 
 const assetUrls = [
     '/',
