d8f5179f27685b232e26d9070707e6c4ff7e04a5
Алексей Егоров <egorovaleksey0@mail.ru>
2021-05-14 13:19:36 +0300

    profile & main page

--- public/main.js
+++ public/main.js
@@ -25,6 +25,7 @@ import '@/static/mediaSccs/registrationMedia.scss';
 import '@/static/mediaSccs/profileMedia.scss';
 import '@/static/mediaSccs/ordersMedia.scss';
 import '@/static/mediaSccs/navbarMedia.scss';
+import '@/static/mediaSccs/mainMedia.scss';
 
 import {
     LoginController,
